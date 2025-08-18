b372d6186a837b0987109f7caef6392412305575
Your Name <Hariharanath247@gmail.com>
2025-08-18 19:11:27 +0530

    removed unwanted files and directories and added new ap image

--- ai/src/App.tsx
+++
@@ -1,7 +0,0 @@
-import NotesPage from "./components/NotesPage";
-
-function App() {
-  return <NotesPage />;
-}
-
-export default App;
--- model/__init__.py
+++
@@ -1,5 +0,0 @@
-# model/__init__.py
-
-from .ml_notes_finder_model import MLNotesFinderModel
-
-__all__ = ["MLNotesFinderModel"]
